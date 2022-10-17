f0d372ddaee383e47f5fe7d8e7d2e50b73da35d8
sin9173 <sid9173@gmail.com>
2022-10-17 21:43:51 +0900

    페이징 수정

--- src/component/Page.js
+++ src/component/Page.js
@@ -5,24 +5,30 @@ class Page extends React.Component {
         searchLink : window.location.search
     }
 
+    move(page) {
+        const urlSearch = new URLSearchParams(window.location.search);
+        urlSearch.set('page', page);
+        
+        window.location.href=`${this.props.listLink}?${urlSearch.toString()}`;
+    }
+
     render() {
         const result = [];
-        const listLink = this.props.listLink;
 
         const startPage = this.props.startPage;
         const endPage = this.props.endPage;
         const lastPage = this.props.lastPage;
         const currentPage = this.props.currentPage;
 
-        if(startPage>10) result.push(<li key="<" className="pageObject"><a href={`${listLink}?page=${startPage-1}${this.state.searchLink.replace(`?page=${currentPage}`, '').replace('?', '&')}`}>{"<"}</a></li>)
+        if(startPage>10) result.push(<li key="<" className="pageObject"><a onClick={this.move.bind(this, (startPage-1))}>{"<"}</a></li>)
 
         for(let i=startPage ; i<=endPage ; i++) {
             if(i>lastPage) break;
-            if(i!=currentPage) result.push(<li key={i} className="pageObject"><a href={`${listLink}?page=${i}${this.state.searchLink.replace(`?page=${currentPage}`, '').replace('?', '&')}`}>{i}</a></li>);
+            if(i!=currentPage) result.push(<li key={i} className="pageObject"><a onClick={this.move.bind(this, i)}>{i}</a></li>);
             if(i==currentPage) result.push(<li key={i} className="pageObject"><span className="active">{i}</span></li>);
         }
 
-        if(endPage<lastPage) result.push(<li key="<" className="pageObject"><a href={`${listLink}?page=${endPage+1}${this.state.searchLink.replace(`?page=${currentPage}`, '').replace('?', '&')}`}>{">"}</a></li>);
+        if(endPage<lastPage) result.push(<li key="<" className="pageObject"><a onClick={this.move.bind(this, (endPage+1))}>{">"}</a></li>);
 
         return <div className="pageController">
             <ul className="pages">
